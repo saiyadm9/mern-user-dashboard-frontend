226a14b1dc8ba3d8576c6998ee0a6c49d1c0976c
saiyadm99 <niloyaali53@gmail.com>
2023-11-14 15:54:37 +0600

    success button style fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://mern-stack-dashboard-api.cyclic.app';
-// export const BASE_URL = 'http://localhost:3001';
+// export const BASE_URL = 'https://mern-stack-dashboard-api.cyclic.app';
+export const BASE_URL = 'http://localhost:3001';
